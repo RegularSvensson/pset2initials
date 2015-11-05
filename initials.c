a36a5160b799260bce2143d29d295f034b4fa429
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-05 17:56:11 +0100

    Remove non-refactored code

--- initials.c
+++ initials.c
@@ -9,22 +9,5 @@ int main(void)
 {
     // get name as input
     string name = GetString();
-    printf("hello, %s\n", name);
     
-    // get first initial
-    char initialOne = name[0];
-    
-    // find blankspace
-    int i = 0;
-    while(i < strlen(name))
-    {
-        if (name[i] == ' ')
-        {
-            break;
-        }
-        i++;
-    }
-    
-    // get second initial
-    char initialTwo = name[i + 1];
 }
